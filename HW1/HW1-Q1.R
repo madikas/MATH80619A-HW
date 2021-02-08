f3aa785f92a3abd172c7e7517d1abe69349e04cd
MadiKas <kasmadius@gmail.com>
2021-02-08 18:41:10 +0600

    HW1 complete

--- HW1/HW1-Q1.R
+++ HW1/HW1-Q1.R
@@ -1,12 +1,12 @@
 library(glmnet)
 library(caret)
-source("C://HEC/Advanced StatLearning/HW/MATH80619A-HW/HW1/ames_preprocess.R")
+source("ames_preprocess.R")
 #a.Use the R library caret to simultaneously optimize the a and l parameters by cross
 #validation in elastic net regression. Compute the MSE and MAE of the Ames data
 #using elastic net regression with optimal parameters.
 set.seed(123456)
 alpha.grid <- seq(0, 1, 0.01)
-srchGrd = expand.grid(.alpha = alpha.grid, .lambda = "all")
+srchGrd <- expand.grid(.alpha = alpha.grid, .lambda = "all")
 elnet.fit <- train(
   Sale_Price ~., data = amesdumtrain, method = "glmnet",
   trControl = trainControl("cv", number = 10),
@@ -20,28 +20,62 @@ optimal.elnet.fit <- glmnet(xdumtrain, amesdumtrain$Sale_Price, alpha = optimal.
 
 predoptimalelnet <- predict(optimal.elnet.fit, newx=xdumtest)
 
-results <- data.frame("Optimal Elastic Net", mean((predoptimalelnet-amesdumtest$Sale_Price)^2), 
-                      mean(abs(predoptimalelnet-amesdumtest$Sale_Price)))
-names(results) <- c("Model", "MSE", "MAE")
-
-results
+Q1results <- data.frame("Optimal Elastic Net Caret", mean((predoptimalelnet-amesdumtest$Sale_Price)^2), 
+                      mean(abs(predoptimalelnet-amesdumtest$Sale_Price)), optimal.alpha, optimal.lambda)
+names(Q1results) <- c("Model", "MSE", "MAE", "Alpha", "Lambda")
 
 #b.Write your own code in order to simultaneously optimize the a and l parameters by
 #cross validation. Compute the MAE and MSE on the Ames data and compare your
 #answer with a).
-#Randomly shuffle the data
-amesdata<-ames[sample(nrow(ames)),]
 
-#Create 10 equally size folds
-folds <- cut(seq(1,nrow(amesdata)),breaks=10,labels=FALSE)
-
-#Perform 10 fold cross validation
-for(i in 1:10){
-  #Segment data by fold using the which() function 
-  testIndexes <- which(folds==i,arr.ind=TRUE)
-  testData <- amesdata[testIndexes, ]
-  trainData <- amesdata[-testIndexes, ]
-  #Use the test and train data partitions however you desire...
+cv.optimalElasticNet <- function(seed, x, y, fold, alpha, lambda, xtrain, ytrain, xtest, ytest) {
+  
+  set.seed(seed)
+  
+  #Create equally sized folds
+  folds <- cut(seq(1,nrow(x)),breaks=fold,labels=FALSE)
+  #alpha lambda grid search
+  searchgrid <- expand.grid(alpha, lambda)
+  grid <- nrow(searchgrid)
+  #Store intermediate results for MSE and MAE for alpha-lambda combinations
+  tuneResults <- cbind(searchgrid, data.frame(matrix(0, ncol = fold, nrow=grid)))
+  #Perform cross validation to tune alpha and lambda based on MSE like caret does
+  for (i in 1:grid) {
+    for(j in 1:fold){
+      #Segment data by fold using the which() function 
+      testIndexes <- which(folds==j,arr.ind=TRUE)
+      testx <- x[testIndexes, ]
+      testy <- y[testIndexes]
+      trainx <- x[-testIndexes, ]
+      trainy <- y[-testIndexes]
+      #Fit and test the alpha-lambda combination for elastic net
+      model.fit <- glmnet(trainx , trainy , alpha = tuneResults[i,1] , lambda = tuneResults[i,2])
+      model.pred <- predict(model.fit , testx , s=model.fit$lambda)
+      meanSquaredError <- mean((model.pred - testy)^2)
+      
+      tuneResults[i, j+2] <- meanSquaredError
+    }
+  }
+  #Calculate average MSE and use that alpha-lambda for optimal model
+  tuneResults$MSE_AVG <- rowMeans(tuneResults[,3:(3+fold-1)])
+  optimalAlpha <- tuneResults[which(tuneResults$MSE_AVG==min(tuneResults$MSE_AVG)), 1]
+  optimalLambda <- tuneResults[which(tuneResults$MSE_AVG==min(tuneResults$MSE_AVG)), 2]
+  
+  optimal.model.fit <- glmnet(xtrain , ytrain , alpha = optimalAlpha , lambda = optimalLambda)
+  optimal.model.pred <- predict(optimal.model.fit , xtest , s=model.fit$lambda)
+  
+  optimalMSE <- mean((optimal.model.pred - ytest)^2)
+  optimalMAE <- mean(abs(optimal.model.pred - ytest))
+  
+  optimalResult <- data.frame("Optimal Elastic Net No Caret", optimalMSE, 
+                              optimalMAE, optimalAlpha, optimalLambda)
+  names(optimalResult) <- c("Model", "MSE", "MAE", "Alpha", "Lambda")
+  return(optimalResult)
 }
 
-#c.Compare the results in a) and b) with the other methods (see slide 83)
+#Run 10fold CV Elastc Net
+
+lambda.grid <- seq(1,3, 0.1)
+ElasticNetNoCaret <- cv.optimalElasticNet(123456, xdum, amesdum$Sale_Price, 10, alpha.grid, lambda.grid, xdumtrain, amesdumtrain$Sale_Price, xdumtest, amesdumtest$Sale_Price)
+Q1results[nrow(Q1results) + 1,] = list(Model=ElasticNetNoCaret$Model, MSE=ElasticNetNoCaret$MSE, 
+                                       MAE=ElasticNetNoCaret$MAE, Alpha=ElasticNetNoCaret$Alpha, Lambda=ElasticNetNoCaret$Lambda)
--- HW1/HW1-Q2.R
+++ HW1/HW1-Q2.R
@@ -2,20 +2,21 @@ library(glmnet)
 library(caret)
 library(MASS)
 #Read datasets
-trainset <- read.csv("C://HEC/Advanced StatLearning/HW/MATH80619A-HW/HW1/data/music_origin_lat_train_set.csv", header=TRUE) 
-testset <- read.csv("C://HEC/Advanced StatLearning/HW/MATH80619A-HW/HW1/data/music_origin_lat_test_set.csv", header=TRUE)
+trainset <- read.csv("data/music_origin_lat_train_set.csv", header=TRUE) 
+testset <- read.csv("data/music_origin_lat_test_set.csv", header=TRUE)
 #Subsets of x
 xtrain <- trainset[,1:68]
 xtest <- testset[,1:68]
-
+musicset <- rbind(trainset, testset)
+xmusicset <- musicset[,1:68]
 #OLS with all variables as benchmark
 lm.fit <- lm(y~., data=trainset)
 
 lm.predict <- predict(lm.fit, newdata=testset)
 #Aggregated data frame of different models' performance
-results <- data.frame("OLS", mean((lm.predict-testset$y)^2), 
+Q2results <- data.frame("OLS", mean((lm.predict-testset$y)^2), 
                       mean(abs(lm.predict-testset$y)))
-names(results) <- c("Model", "MSE", "MAE")
+names(Q2results) <- c("Model", "MSE", "MAE")
 
 #Stepwise, forward, backward based on AIC
 set.seed(123)
@@ -29,11 +30,11 @@ backward.predict <- predict(backward, newdata = testset)
 forward.predict <- predict(forward, newdata = testset)
 stepwise.predict <- predict(stepwise, newdata = testset)
 
-results[nrow(results) + 1,] = list(Model="backward", MSE=mean((backward.predict-testset$y)^2), 
+Q2results[nrow(Q2results) + 1,] = list(Model="backward", MSE=mean((backward.predict-testset$y)^2), 
                                    MAE=mean(abs(backward.predict-testset$y)))
-results[nrow(results) + 1,] = list(Model="forward", MSE=mean((forward.predict-testset$y)^2), 
+Q2results[nrow(Q2results) + 1,] = list(Model="forward", MSE=mean((forward.predict-testset$y)^2), 
                                    MAE=mean(abs(forward.predict-testset$y)))
-results[nrow(results) + 1,] = list(Model="stepwise", MSE=mean((stepwise.predict-testset$y)^2), 
+Q2results[nrow(Q2results) + 1,] = list(Model="stepwise", MSE=mean((stepwise.predict-testset$y)^2), 
                                    MAE=mean(abs(stepwise.predict-testset$y)))
 
 #Ridge Regression
@@ -42,7 +43,7 @@ plot(ridge.fit)
 
 predridge <- predict(ridge.fit, new=as.matrix(xtest), s="lambda.min")
 
-results[nrow(results) + 1,] = list(Model="Ridge Regression", MSE=mean((predridge-testset$y)^2), 
+Q2results[nrow(Q2results) + 1,] = list(Model="Ridge Regression", MSE=mean((predridge-testset$y)^2), 
                                    MAE=mean(abs(predridge-testset$y)))
 
 #Lasso Regression
@@ -51,7 +52,7 @@ plot(lasso.fit)
 
 predlasso <- predict(lasso.fit, new=as.matrix(xtest), s="lambda.min")
 
-results[nrow(results) + 1,] = list(Model="Lasso Regression", MSE=mean((predlasso-testset$y)^2), 
+Q2results[nrow(Q2results) + 1,] = list(Model="Lasso Regression", MSE=mean((predlasso-testset$y)^2), 
                                    MAE=mean(abs(predlasso-testset$y)))
 
 #Elastic net with alpha=0.5, 0.2 and 0.8
@@ -64,18 +65,21 @@ predelnet0.5 <- predict(elnet0.5.fit, new=as.matrix(xtest), s="lambda.min")
 predelnet0.8 <- predict(elnet0.8.fit, new=as.matrix(xtest), s="lambda.min")
 
 
-results[nrow(results) + 1,] = list(Model="Elastic Net Alpha=0.2", MSE=mean((predelnet0.2-testset$y)^2), 
+Q2results[nrow(Q2results) + 1,] = list(Model="Elastic Net Alpha=0.2", MSE=mean((predelnet0.2-testset$y)^2), 
                                    MAE=mean(abs(predelnet0.2-testset$y)))
-results[nrow(results) + 1,] = list(Model="Elastic Net Alpha=0.5", MSE=mean((predelnet0.5-testset$y)^2), 
+Q2results[nrow(Q2results) + 1,] = list(Model="Elastic Net Alpha=0.5", MSE=mean((predelnet0.5-testset$y)^2), 
                                    MAE=mean(abs(predelnet0.5-testset$y)))
-results[nrow(results) + 1,] = list(Model="Elastic Net Alpha=0.8", MSE=mean((predelnet0.8-testset$y)^2), 
+Q2results[nrow(Q2results) + 1,] = list(Model="Elastic Net Alpha=0.8", MSE=mean((predelnet0.8-testset$y)^2), 
                                    MAE=mean(abs(predelnet0.8-testset$y)))
 
 #Elastic net with optimal alpha and lambda using caret
+alpha.grid <- seq(0, 1, 0.01)
+srchGrd = expand.grid(.alpha = alpha.grid, .lambda = "all")
+
 elnet.fit <- train(
   y ~., data = trainset, method = "glmnet",
   trControl = trainControl("cv", number = 10),
-  tuneLength = 10
+  tuneLength = 10, tuneGrid = srchGrd
 )
 
 optimal.alpha  <- elnet.fit$bestTune$alpha 
@@ -85,8 +89,59 @@ optimal.elnet.fit <- glmnet(as.matrix(xtrain), trainset$y, alpha = optimal.alpha
 
 predoptimalelnet <- predict(optimal.elnet.fit, newx=as.matrix(xtest))
 
-results[nrow(results) + 1,] = list(Model="Optimal Elastic Net", MSE=mean((predoptimalelnet-testset$y)^2), 
+Q2results[nrow(Q2results) + 1,] = list(Model="Optimal Elastic Net Caret", MSE=mean((predoptimalelnet-testset$y)^2), 
                                    MAE=mean(abs(predoptimalelnet-testset$y)))
+
+#Elastic net with own function 
+cv.optimalElasticNet <- function(seed, x, y, fold, alpha, lambda, xtrain, ytrain, xtest, ytest) {
+  
+  set.seed(seed)
+  #Create equally sized folds
+  folds <- cut(seq(1,nrow(x)),breaks=fold,labels=FALSE)
+  #alpha lambda grid search
+  searchgrid <- expand.grid(alpha, lambda)
+  grid <- nrow(searchgrid)
+  #Store intermediate results for MSE and MAE for alpha-lambda combinations
+  tuneResults <- cbind(searchgrid, data.frame(matrix(0, ncol = fold , nrow=grid)))
+  #Perform cross validation to tune alpha and lambda based on MSE like caret does
+  for (i in 1:grid) {
+    for(j in 1:fold){
+      #Segment data by fold using the which() function 
+      testIndexes <- which(folds==j,arr.ind=TRUE)
+      testx <- x[testIndexes, ]
+      testy <- y[testIndexes]
+      trainx <- x[-testIndexes, ]
+      trainy <- y[-testIndexes]
+      #Fit and test the alpha-lambda combination for elastic net
+      model.fit <- glmnet(as.matrix(trainx) , trainy , alpha = tuneResults[i,1] , lambda = tuneResults[i,2])
+      model.pred <- predict(model.fit , as.matrix(testx) , s=model.fit$lambda)
+      meanSquaredError <- mean((model.pred - testy)^2)
+      
+      tuneResults[i, j+2] <- meanSquaredError
+    }
+  }
+  #Calculate average MSE and use that alpha-lambda for optimal model
+  tuneResults$MSE_AVG <- rowMeans(tuneResults[,3:(3+fold-1)])
+  optimalAlpha <- tuneResults[which(tuneResults$MSE_AVG==min(tuneResults$MSE_AVG)), 1]
+  optimalLambda <- tuneResults[which(tuneResults$MSE_AVG==min(tuneResults$MSE_AVG)), 2]
+  
+  optimal.model.fit <- glmnet(as.matrix(xtrain) , ytrain , alpha = optimalAlpha , lambda = optimalLambda)
+  optimal.model.pred <- predict(optimal.model.fit , as.matrix(xtest) , s=model.fit$lambda)
+  
+  optimalMSE <- mean((optimal.model.pred - ytest)^2)
+  optimalMAE <- mean(abs(optimal.model.pred - ytest))
+  
+  #optimalResult <- list("Optimal Elastic Net No Caret", optimalMSE, optimalMAE)
+  optimalResult <- data.frame("Optimal Elastic Net No Caret", optimalMSE, 
+                              optimalMAE)
+  names(optimalResult) <- c("Model", "MSE", "MAE")
+  return(optimalResult)
+}
+
+alpha.grid <- seq(0, 1, 0.01)
+lambda.grid <- seq(1,3, 0.1)
+ElasticNetNoCaret <- cv.optimalElasticNet(123456, xmusicset, musicset$y, 10, alpha.grid, lambda.grid, xtrain, trainset$y, xtest, testset$y)
+Q2results[nrow(Q2results) + 1,] = list(Model=ElasticNetNoCaret$Model,MSE=ElasticNetNoCaret$MSE, MAE=ElasticNetNoCaret$MAE)
 
 
 #Relax Lasso Regression
@@ -94,7 +149,6 @@ relax.lasso.fit <- cv.glmnet(as.matrix(xtrain), trainset$y, alpha = 1, relax=TRU
 
 predrelaxlasso <- predict(relax.lasso.fit, new=as.matrix(xtest), s="lambda.min", gamma="gamma.min")
 
-results[nrow(results) + 1,] = list(Model="Relaxed Lasso", MSE=mean((predrelaxlasso-testset$y)^2), 
+Q2results[nrow(Q2results) + 1,] = list(Model="Relaxed Lasso", MSE=mean((predrelaxlasso-testset$y)^2), 
                                    MAE=mean(abs(predrelaxlasso-testset$y)))
 
-results
